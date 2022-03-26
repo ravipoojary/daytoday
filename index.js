e3ff4d5b240d28bbabcd7bafaa09acb72d1cf564
ravipoojary <99193908+ravipoojary@users.noreply.github.com>
2022-03-26 11:34:27 +0530

    day to day

---
+++ index.js
@@ -0,0 +1,17 @@
+function one(){
+    let pic=document.getElementById('pic').src="images/img.jpg";
+}
+function two(){
+    let pic=document.getElementById('pic').src="images/img2.jpg";
+}
+function three(){
+    let pic=document.getElementById('pic').src="images/img3.jpg";
+}
+function four(){
+    let pic=document.getElementById('pic').src="images/img1.jpg";
+  
+}
+setInterval(one,3000)
+setInterval(two,6000)
+setInterval(three,9000)
+setInterval(four,12000)
